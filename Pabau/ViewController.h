53082ce3780ed8ad70ce69872166078f72351776
timothy <muhorotimothy5@gmail.com>
2024-05-14 01:33:03 +0800

    terminal screen

--- Pabau/ViewController.h
+++ Pabau/ViewController.h
@@ -8,55 +8,13 @@
 #import <UIKit/UIKit.h>
 
 
-@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+@interface ViewController : UIViewController
 
-typedef NS_ENUM(NSInteger, PaymentStatus) {
-    COMPLETED,
-    UNPAID,
-    PART_PAID
-};
+@property (weak, nonatomic) IBOutlet UIButton *btnRequestTerminal;
+@property (weak, nonatomic) IBOutlet UIButton *btnLearnMore;
 
-@property (weak, nonatomic) IBOutlet UIView *viewLeft;
-@property (weak, nonatomic) IBOutlet UIView *viewRight;
-
-
-@property (weak, nonatomic) IBOutlet UIImageView *imgLogo;
-@property (weak, nonatomic) IBOutlet UILabel *lblAmount;
-@property (weak, nonatomic) IBOutlet UILabel *lblTotal;
-
-
-@property (weak, nonatomic) IBOutlet UIButton *btnBookNextAppointment;
-@property (weak, nonatomic) IBOutlet UIButton *btnMail;
-@property (weak, nonatomic) IBOutlet UIButton *btnMessage;
-@property (weak, nonatomic) IBOutlet UIButton *btnPrint;
-
-@property (weak, nonatomic) IBOutlet UIView *viewNextAppointment;
-
-@property (weak, nonatomic) IBOutlet UIView *viewStatus;
-@property (weak, nonatomic) IBOutlet UILabel *lblStatus;
-
-
-@property (weak, nonatomic) IBOutlet UITableView *productTable;
-@property (weak, nonatomic) IBOutlet UITableView *paymentsTable;
-
-
-
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *landscapeLeftViewTrailingConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *landscapeLeftViewBottomConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *landscapeRightViewTopConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *portraitLeftViewTrailingConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *portraitRightViewLeadingConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *portraitLeftViewBottomConstraint;
-
-@property (nonatomic, strong) NSArray *paymentTypes;
-@property (nonatomic, strong) NSArray *paymentAmounts;
-
-- (IBAction)onBookNextAppointment:(id)sender;
-- (IBAction)onBtnMail:(id)sender;
-- (IBAction)onBtnMessage:(id)sender;
-- (IBAction)onBtnPrint:(id)sender;
-
-- (void) changeStatus:(PaymentStatus)status;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *backgroundImageWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textWidthConstraint;
 
 @end
 
--- Pabau/ViewController.m
+++ Pabau/ViewController.m
@@ -6,8 +6,6 @@
 //
 
 #import "ViewController.h"
-#import "ItemTableViewCell.h"
-#import "PaymentTableViewCell.h"
 
 @interface ViewController ()
 
@@ -17,30 +15,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
-    self.paymentTypes = @[@"Subtotal", @"Discount", @"Tax", @"Cash", @"Card", @"Bank Transfer", @"Total"];
-    self.paymentAmounts = @[@"£181.50", @"£0", @"£4.33", @"£10.00", @"10.00", @"£161.50", @"£181.50"];
+    self.btnRequestTerminal.layer.borderWidth = 1.0f;
+    self.btnRequestTerminal.layer.cornerRadius = 4.0f;
     
-    // For Products list
-    self.productTable.dataSource = self;
-    self.productTable.delegate = self;
-    self.productTable.contentInset = UIEdgeInsetsZero;
-    self.productTable.scrollIndicatorInsets = UIEdgeInsetsZero;
-        
-    // For Payments list
-    self.paymentsTable.dataSource = self;
-    self.paymentsTable.delegate = self;
-    self.paymentsTable.contentInset = UIEdgeInsetsZero;
-    self.paymentsTable.scrollIndicatorInsets = UIEdgeInsetsZero;
+    self.btnLearnMore.layer.cornerRadius = 4.0f;
+    self.btnLearnMore.layer.borderWidth = 1.0f;
+    self.btnLearnMore.layer.borderColor = [UIColor grayColor].CGColor;
     
-    
-//    // hide Next Appointment
-//    [self.viewNextAppointment setHidden:YES];
-    
-    [self changeStatus: COMPLETED];
-    
-    [self.btnMessage setHidden:YES];
     
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
@@ -93,151 +75,19 @@
             break;
     }
     
-    if (!isUnknown) {
-        [self.landscapeLeftViewTrailingConstraint setActive:isLandscape];
-        [self.landscapeLeftViewBottomConstraint setActive:isLandscape];
-        [self.landscapeRightViewTopConstraint setActive:isLandscape];
+    if (isLandscape) {
+        self.textWidthConstraint.constant = 0.9f;
+        self.backgroundImageWidthConstraint.constant = 0.65f;
         
-        [self.portraitLeftViewBottomConstraint setActive:!isLandscape];
-        [self.portraitLeftViewTrailingConstraint setActive:!isLandscape];
-        [self.portraitRightViewLeadingConstraint setActive:!isLandscape];
-        
-        [self updateViewConstraints];
-        [self.view layoutIfNeeded];
+    } else {
+        self.textWidthConstraint.constant = 0.65f;
+        self.backgroundImageWidthConstraint.constant = 0.45f;
     }
     
-}
-
-
-
-- (IBAction)onBtnPrint:(id)sender {
-}
-
-- (IBAction)onBtnMessage:(id)sender {
-}
-
-- (IBAction)onBtnMail:(id)sender {
-}
-
-- (IBAction)onBookNextAppointment:(id)sender {
-}
-
--(void) changeStatus:(PaymentStatus) status {
+    [self updateViewConstraints];
+    [self.view layoutIfNeeded];
     
-    UIColor *redColor = [UIColor colorWithRed:1.0f green:91/255.0 blue:100/255.0 alpha:1.0];
-    UIColor *greenColor = [UIColor colorWithRed:101/255.0 green:205/255.0 blue:152/255.0 alpha:1.0];
-        
-    UIColor *backgroundColor = [UIColor colorWithRed:246/255.0f green:1.0f blue:237/255.0f alpha:1.0];
-    UIColor *borderColor = [UIColor colorWithRed:183/255.0f green:235/255.0f blue:143/255.0f alpha:1.0f];
-    UIColor *textColor = [UIColor colorWithRed:82/255.0f green:196/255.0f blue:26/255.0f alpha:1.0];
-    
-    switch (status) {
-        case COMPLETED:
-            // left
-            [self.imgLogo setImage:[UIImage imageNamed:@"success_icon"]];
-            [self.lblAmount setTextColor:greenColor];
-            [self.btnBookNextAppointment setTitle:@"Book Next Appointment" forState:UIControlStateNormal];
-            
-            // right
-            [self.lblStatus setText:@"Completed"];
-            break;
-        case UNPAID:
-            // left
-            [self.imgLogo setImage:[UIImage imageNamed:@"payment_icon"]];
-            [self.lblAmount setTextColor:redColor];
-            [self.btnBookNextAppointment setTitle:@"Add A Payment" forState:UIControlStateNormal];
-            
-            // right
-            backgroundColor = [UIColor colorWithRed:255/255.0f green:241/255.0f blue:240/255.0f alpha:1.0];
-            borderColor = [UIColor colorWithRed:255/255.0f green:163/255.0f blue:158/255.0f alpha:1.0f];
-            textColor = [UIColor colorWithRed:245/255.0f green:34/255.0f blue:45/255.0f alpha:1.0];
-            
-            [self.lblStatus setText:@"Unpaid"];
-            break;
-        case PART_PAID:
-            // left
-            [self.imgLogo setImage:[UIImage imageNamed:@"payment_icon"]];
-            [self.lblAmount setTextColor:redColor];
-            [self.btnBookNextAppointment setTitle:@"Add A Payment" forState:UIControlStateNormal];
-            
-            // right
-            backgroundColor = [UIColor colorWithRed:255/255.0f green:247/255.0f blue:230/255.0f alpha:1.0];
-            borderColor = [UIColor colorWithRed:255/255.0f green:213/255.0f blue:145/255.0f alpha:1.0f];
-            textColor = [UIColor colorWithRed:250/255.0f green:140/255.0f blue:22/255.0f alpha:1.0];
-            
-            [self.lblStatus setText:@"Part-Paid"];
-            break;
-        default:
-            break;
-    }
-        
-    [self.btnBookNextAppointment.layer setCornerRadius:4.0f];
-    
-    [self.lblStatus setTextColor:textColor];
-    [self.viewStatus setBackgroundColor:backgroundColor];
-    [self.viewStatus.layer setBorderColor:borderColor.CGColor];
-    
-    [self.viewStatus.layer setBorderWidth:1.0f];
-    [self.viewStatus.layer setCornerRadius: 2.0f];
 }
 
-
-#pragma TableView Datasouce
-
-- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] init];
-        
-    if (tableView == self.productTable) {
-        ItemTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ItemTableViewCellIdentifier"];
-        if (cell == nil) {
-            cell = [[ItemTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"ItemTableViewCellIdentifier"];
-        }
-        cell.lblItemName.text = @"Hydrafacial";
-        cell.lblFrom.text = @"With Mel Pabau";
-        cell.lblAmount.text = @"£120.00";
-        cell.lblNumber.text = @"1";
-        return cell;
-    } else if (tableView == self.paymentsTable) {
-        PaymentTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PaymentTableViewCellIdentifier"];
-        if (cell == nil) {
-            cell = [[PaymentTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"PaymentTableViewCellIdentifier"];
-        }
-        cell.lblPaymentType.text = self.paymentTypes[indexPath.row];
-        cell.lblAmount.text = self.paymentAmounts[indexPath.row];
-
-        if (indexPath.row == (self.paymentTypes.count - 1)) {
-            UIColor *textColor = [UIColor colorWithRed:61/255.0f green:61/255.0f blue:70/255.0f alpha:1.0];
-            UIFont *font = cell.lblPaymentType.font;
-            font = [UIFont systemFontOfSize:font.pointSize + 1 weight:UIFontWeightMedium];
-            [cell.lblPaymentType setFont:font];
-            [cell.lblPaymentType setTextColor:textColor];
-            
-            [cell.lblAmount setFont:font];
-            [cell.lblAmount setTextColor:textColor];
-        }
-        return cell;
-    }
-    return cell;
-}
-
-#pragma TableviewDelegate
-- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (tableView == self.productTable) {
-        return 2;
-    } else if (tableView == self.paymentsTable) {
-        return self.paymentTypes.count;
-    }
-    return 0;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (tableView == self.productTable) {
-        return 70.0f;
-    } else if (tableView == self.paymentsTable) {
-        return 30.0f;
-    }
-    return 0.0;
-}
 
 @end
